202b79fccf6d16bb7e0996e9308ed73c59e8e624
bruma <brumam@roehampton.ac.uk>
2023-04-13 22:52:34 +0100

    22:52  date 13/04/2023 -  / Sign -up page in progress / Sign in page Completed / OTP COMPLETED  / RESET PASSWORD PAGE COMPLETED/ CHANGE PASSWORD SUCCESSFULLY COMPLETED
    activity_main.xml is created and now I start to work on Java

--- app/src/main/java/com/example/e_mentor/WelcomeActivity.java
+++ app/src/main/java/com/example/e_mentor/WelcomeActivity.java
@@ -3,6 +3,9 @@ package com.example.e_mentor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        Button signInButton = findViewById(R.id.button_signin);
+        Button signUpButton = findViewById(R.id.button_signup);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(WelcomeActivity.this, Login.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent = new Intent(WelcomeActivity.this, SignUp.class);
+                startActivity(signUpIntent);
+            }
+        });
     }
 }
